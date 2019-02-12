003da42d10e81bc8f26e546e6abb6104878427d3
Jesus Salvador Cortes Gutierrez <31747651+noxtla@users.noreply.github.com>
2019-02-12 13:45:02 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,34 @@
+package Condicionales;
 
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+	int numero,random = 0 , intentos =0;
+	int band =1;
+	
+
+
+	 do {	
+		 
+		 numero = Integer.parseInt(JOptionPane.showInputDialog("Ingresa el numero "));
+
+		 	
+		 	if (numero > 0) {
+		    random += numero;
+		    intentos ++;
+		 	}
+		 	
+		 	if (numero < 0) {
+				band = 0;
+			}
+		 	
+				
+				
+		} while (band != 0);
+		
+		
+		JOptionPane.showMessageDialog(null, "El promedio es " + random/intentos);
+	}
+}
